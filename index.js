544b645c6a7569823d76810b1e8b1c805b82310f
modiribrodie <modiribrodi@gmail.com>
2024-04-25 02:40:05 +0200

    Customized app Javascript
    Created a Realtime Database.
    Used the Firebase function to push inputValue to the database.

--- index.js
+++ index.js
@@ -2,6 +2,16 @@
 Challenge:
 Make it so that when you click the 'Add to cart' button, whatever is written in the input field should be console logged.
 */
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+
+const appSettings = {
+    databaseURL: "https://realtime-database-db273-default-rtdb.europe-west1.firebasedatabase.app/"
+}
+
+const app = initializeApp(appSettings)
+const database = getDatabase(app)
+const shoppingListInDB = ref(database, "shoppingList")
 
 const inputFieldEl = document.getElementById("input-field")
 const addButtonEl = document.getElementById("add-button")
@@ -9,5 +19,7 @@ const addButtonEl = document.getElementById("add-button")
 addButtonEl.addEventListener("click", function() {
     let inputValue = inputFieldEl.value
     
+    push(shoppingListInDB, inputValue)
+
     console.log(inputValue)
 })
